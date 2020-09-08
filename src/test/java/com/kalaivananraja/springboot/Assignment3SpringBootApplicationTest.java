fe49afe933f54b1e60a087fce8ff4e635a41f4aa
Kalaivanan-Raja <70875089+Kalaivanan-Raja@users.noreply.github.com>
2020-09-08 16:39:48 +0530

    JUnit test case

---
+++ src/test/java/com/kalaivananraja/springboot/Assignment3SpringBootApplicationTest.java
@@ -0,0 +1,24 @@
+package com.kalaivananraja.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Kalaivanan Raja
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Assignment3SpringBootApplicationTest {
+
+	/**
+	 * test the Assignment3SpringBootApplication
+	 */
+	@Test
+	public void contextLoads() {
+		
+	}
+}
