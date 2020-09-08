646b1f7f371a285f3ed31e3e9609cad0b2466c83
Kalaivanan-Raja <70875089+Kalaivanan-Raja@users.noreply.github.com>
2020-09-08 12:50:36 +0530

    javadoc

--- src/main/java/com/kalaivananraja/springboot/Assignment3SpringBootApplication.java
+++ src/main/java/com/kalaivananraja/springboot/Assignment3SpringBootApplication.java
@@ -3,9 +3,18 @@ package com.kalaivananraja.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * starts the serverlet for these project
+ * @author Kalaivanan Raja
+ *
+ */
 @SpringBootApplication
 public class Assignment3SpringBootApplication {
 
+	/**
+	 * it invoke run method to initialize the servelet
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment3SpringBootApplication.class, args);
 	}
