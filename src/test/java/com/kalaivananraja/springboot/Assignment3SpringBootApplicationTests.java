3cf5b5d49f3855092595a7d887a0030af83519d1
Kalaivanan-Raja <70875089+Kalaivanan-Raja@users.noreply.github.com>
2020-09-08 12:28:08 +0530

    java doc for junit

--- src/test/java/com/kalaivananraja/springboot/Assignment3SpringBootApplicationTests.java
+++ src/test/java/com/kalaivananraja/springboot/Assignment3SpringBootApplicationTests.java
@@ -3,6 +3,12 @@ package com.kalaivananraja.springboot;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Test's the Assignment3SpringBootApplication
+ * 
+ * @author Kalaivanan Raja
+ *
+ */
 @SpringBootTest
 public class Assignment3SpringBootApplicationTests {
 
