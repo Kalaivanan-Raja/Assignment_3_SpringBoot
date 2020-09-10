23172104ba49e19b0858fd915b4beaebb6d91135
Kalaivanan-Raja <70875089+Kalaivanan-Raja@users.noreply.github.com>
2020-09-10 15:29:22 +0530

    junit test case with javadoc

---
+++ src/test/java/com/kalaivananraja/springboot/exceltest/ReadExcelFileTest.java
@@ -0,0 +1,18 @@
+package com.kalaivananraja.springboot.exceltest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Kalaivanan Raja
+ *
+ */
+public class ReadExcelFileTest {
+
+	@Test
+	public void testExcelToDb() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/kalaivananraja/springboot/servicetest/ExcelServiceTest.java
@@ -0,0 +1,18 @@
+package com.kalaivananraja.springboot.servicetest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Kalaivanan Raja
+ *
+ */
+public class ExcelServiceTest {
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+}
