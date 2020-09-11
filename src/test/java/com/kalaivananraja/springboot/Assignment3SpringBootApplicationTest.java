ba1acb634b370f3c3cfe104a1e723b52a0afa42b
Kalaivanan-Raja <70875089+Kalaivanan-Raja@users.noreply.github.com>
2020-09-11 17:13:28 +0530

    modified

--- src/test/java/com/kalaivananraja/springboot/Assignment3SpringBootApplicationTest.java
+++ src/test/java/com/kalaivananraja/springboot/Assignment3SpringBootApplicationTest.java
@@ -1,9 +1,16 @@
 package com.kalaivananraja.springboot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kalaivananraja.springboot.excel.ReadExcelFile;
+import com.kalaivananraja.springboot.model.BusinessPartner;
+import com.kalaivananraja.springboot.service.ExcelService;
 
 /**
  * @author Kalaivanan Raja
@@ -17,8 +24,14 @@ public class Assignment3SpringBootApplicationTest {
 	/**
 	 * test the Assignment3SpringBootApplication
 	 */
+	@Autowired
+	private ExcelService excelservice;
+	private ReadExcelFile ref;
+	private BusinessPartner bp;
 	@Test
 	public void contextLoads() {
-		
+		assertThat(excelservice).isNotNull();
+		assertThat(ref).isNotNull();
+		assertThat(bp).isNotNull();
 	}
 }
